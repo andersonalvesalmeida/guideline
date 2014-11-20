7eaba87d74837cbd21f22d738bb9f03c64ef3007
Anderson Almeida <poolcity@gmail.com>
2014-11-20 22:57:18 +0000

    add linguagem pt no numeral

--- guideline/js/guideline.js
+++ guideline/js/guideline.js
@@ -33367,40 +33367,7 @@ else if ( jQuery ) {
         makeGlobal();
     }
 }).call(this);
-;/*! 
- * numeral.js language configuration
- * language : portuguese brazil (pt-br)
- * author : Ramiro Varandas Jr : https://github.com/ramirovjr
- */
-(function () {
-    var language = {
-        delimiters: {
-            thousands: '.',
-            decimal: ','
-        },
-        abbreviations: {
-            thousand: 'mil',
-            million: 'milhões',
-            billion: 'b',
-            trillion: 't'
-        },
-        ordinal: function (number) {
-            return 'º';
-        },
-        currency: {
-            symbol: 'R$'
-        }
-    };
-
-    // Node
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = language;
-    }
-    // Browser
-    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
-        this.numeral.language('pt-br', language);
-    }
-}());;/*!
+;/*!
  * numeral.js
  * version : 1.5.3
  * author : Adam Draper
@@ -34079,7 +34046,40 @@ else if ( jQuery ) {
         });
     }
 }).call(this);
-;    function pad(str, max) {
+;/*! 
+ * numeral.js language configuration
+ * language : portuguese brazil (pt-br)
+ * author : Ramiro Varandas Jr : https://github.com/ramirovjr
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'mil',
+            million: 'milhões',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            return 'º';
+        },
+        currency: {
+            symbol: 'R$'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('pt-br', language);
+    }
+}());;    function pad(str, max) {
 		  return str.length < max ? pad("0" + str, max) : str;
 		}
 		
